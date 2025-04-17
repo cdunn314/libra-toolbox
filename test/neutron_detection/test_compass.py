6e72209894d144a8c6810ddf7e735eb747960992
cdunn314 <cdunn314@mit.edu>
2025-04-17 14:28:46 -0400

    Added new test cases to test_compass

--- test/neutron_detection/test_compass.py
+++ test/neutron_detection/test_compass.py
@@ -4,7 +4,9 @@ import os
 from libra_toolbox.neutron_detection.activation_foils import compass
 
 TEST_DIR = os.path.dirname(__file__)  # directory of this test file
-COMPASS_DATA_DIR = os.path.join(TEST_DIR, "compass_data")
+COMPASS_DATA_DIR = os.path.join(TEST_DIR, "compass_test_data")
+RUN_1_TEST_DATA_DIR = os.path.join(COMPASS_DATA_DIR, 'baby_1L_run1_test_data')
+RUN_3_TEST_DATA_DIR = os.path.join(COMPASS_DATA_DIR, 'baby_1L_run3_test_data')
 
 
 @pytest.mark.parametrize("filename, expected_channel", [
@@ -33,11 +35,26 @@ def check_dictionaries(test_dict, expected_dict):
 
 
 @pytest.mark.parametrize("directory, expected_filenames",[
-    (COMPASS_DATA_DIR, {0:["Data_CH0@DT5730SB_Cs137_Problem_Waveform_1103.CSV",
-                         "Data_CH0@DT5730SB_Cs137_Problem_Waveform_1103_1.CSV"],
-                      1:["Data_CH1@V1725_Cs137_Normal_List_241107.CSV",
-                         "Data_CH1@V1725_Cs137_Normal_List_241107_1.CSV",
-                         "Data_CH1@V1725_Cs137_Normal_List_241107_2.CSV"]})
+    (os.path.join(RUN_1_TEST_DATA_DIR, 'Background_20241108_0027/UNFILTERED'),
+     {2:["Data_CH2@V1725_292_Background_20241108_0027.CSV"]}),
+    (os.path.join(RUN_1_TEST_DATA_DIR, 'Cs137_4_66uCi_19Mar2014_20241107/UNFILTERED'), 
+     {2:["Data_CH2@V1725_292_Cs137_4_66uCi_19Mar2014_241107.CSV",
+         "Data_CH2@V1725_292_Cs137_4_66uCi_19Mar2014_241107_1.CSV",
+         "Data_CH2@V1725_292_Cs137_4_66uCi_19Mar2014_241107_2.CSV",
+         "Data_CH2@V1725_292_Cs137_4_66uCi_19Mar2014_241107_3.CSV",
+         "Data_CH2@V1725_292_Cs137_4_66uCi_19Mar2014_241107_4.CSV",
+         "Data_CH2@V1725_292_Cs137_4_66uCi_19Mar2014_241107_5.CSV",
+         "Data_CH2@V1725_292_Cs137_4_66uCi_19Mar2014_241107_6.CSV",
+         "Data_CH2@V1725_292_Cs137_4_66uCi_19Mar2014_241107_7.CSV"]
+        }),
+    (os.path.join(RUN_3_TEST_DATA_DIR, 'Na22_9_98uCi_29Sep2023_250318_run3/UNFILTERED'),
+     {4:["Data_CH4@V1725_292_Na22_9_98uCi_29Sep2023_250318_run3.CSV",
+         "Data_CH4@V1725_292_Na22_9_98uCi_29Sep2023_250318_run3_1.CSV",
+         "Data_CH4@V1725_292_Na22_9_98uCi_29Sep2023_250318_run3_2.CSV"
+         ],
+      5:["Data_CH5@V1725_292_Na22_9_98uCi_29Sep2023_250318_run3.CSV",
+         "Data_CH5@V1725_292_Na22_9_98uCi_29Sep2023_250318_run3_1.CSV",
+         ]})
 ])
 def test_sort_compass_files(directory, expected_filenames):
     data_filenames = compass.sort_compass_files(directory)
