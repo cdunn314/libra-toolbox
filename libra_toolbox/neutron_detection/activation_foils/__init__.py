e6ec09ed3fe7e2cdec6fbc42ec37b9d9c696d91b
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-05-08 14:18:50 -0400

    nuclide class

--- libra_toolbox/neutron_detection/activation_foils/__init__.py
+++ libra_toolbox/neutron_detection/activation_foils/__init__.py
@@ -1,33 +1,18 @@
 from datetime import datetime
+from . import calibration
+
+from dataclasses import dataclass
 
 
+@dataclass
 class CheckSource:
-    nuclide: str
-    energy: list
-    intensity: list
+    nuclide: calibration.Nuclide
     activity_date: datetime.date
     activity: float
-    half_life: float
-
-    def __init__(
-        self,
-        nuclide: str,
-        energy: list,
-        intensity: list,
-        activity_date: datetime.date,
-        activity: float,
-        half_life: float,
-    ):
-        self.nuclide = nuclide
-        self.energy = energy
-        self.intensity = intensity
-        self.activity_date = activity_date
-        self.activity = activity
-        self.half_life = half_life
 
 
 class ActivationFoil:
-    nuclide: str
+    nuclide: calibration.Nuclide
     mass: float
 
 
--- libra_toolbox/neutron_detection/activation_foils/calibration.py
+++ libra_toolbox/neutron_detection/activation_foils/calibration.py
@@ -1,47 +1,46 @@
-import datetime
+from dataclasses import dataclass
+from typing import List
 
-from libra_toolbox.neutron_detection.activation_foils import CheckSource
 
-uCi_to_Bq = 3.7e4
+@dataclass
+class Nuclide:
+    """
+    Class to hold the information of a nuclide.
+    """
 
-check_source_ba133 = CheckSource(
-    nuclide="Ba133",
+    name: str
+    energy: List[float]
+    intensity: List[float]
+    half_life: float
+
+
+ba133 = Nuclide(
+    name="Ba133",
     energy=[80.9979, 276.3989, 302.8508, 356.0129, 383.8485],
     intensity=[0.329, 0.0716, 0.1834, 0.6205, 0.0894],
-    activity_date=datetime.date(2014, 3, 19),
-    activity=1 * uCi_to_Bq,
     half_life=10.551 * 365.25 * 24 * 3600,
 )
-
-check_source_co60 = CheckSource(
-    nuclide="Co60",
+co60 = Nuclide(
+    name="Co60",
     energy=[1173.228, 1332.492],
     intensity=[0.9985, 0.999826],
-    activity_date=datetime.date(2014, 3, 19),
-    activity=0.872 * uCi_to_Bq,
     half_life=1925.28 * 24 * 3600,
 )
-check_source_na22 = CheckSource(
-    nuclide="Na22",
+na22 = Nuclide(
+    name="Na22",
     energy=[511, 1274.537],
     intensity=[1.80, 0.9994],
-    activity_date=datetime.date(2014, 3, 19),
-    activity=5 * uCi_to_Bq,
     half_life=2.6018 * 365.25 * 24 * 3600,
 )
-check_source_cs137 = CheckSource(
-    nuclide="Cs137",
+cs137 = Nuclide(
+    name="Cs137",
     energy=[661.657],
     intensity=[0.851],
-    activity_date=datetime.date(2014, 3, 19),
-    activity=4.66 * uCi_to_Bq,
     half_life=30.08 * 365.25 * 24 * 3600,
 )
-check_source_mn54 = CheckSource(
-    nuclide="Mn54",
+mn54 = Nuclide(
+    name="Mn54",
     energy=[834.848],
     intensity=[0.99976],
-    activity_date=datetime.date(2016, 5, 2),
-    activity=6.27 * uCi_to_Bq,
     half_life=312.20 * 24 * 3600,
 )
