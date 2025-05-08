ecc757d08e4b00d29e40c599fae0854a682ad9a5
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-05-08 11:37:29 -0400

    added classes for check sources

--- libra_toolbox/neutron_detection/activation_foils/calibration.py
+++ libra_toolbox/neutron_detection/activation_foils/calibration.py
@@ -1,39 +1,42 @@
 import datetime
 
-def get_decay_lines(nuclides:list[str])->dict:
-    """ Creates dictionary of check source data
-    given a list of check source nuclides. """
-    # energy is the gamma energy in units of eV
-    # intensity is the percentage of decays that result in this energy gamma
-    all_decay_lines = {'Ba133':{'energy':[80.9979, 276.3989, 302.8508, 356.0129, 383.8485],
-                         'intensity':[0.329, 0.0716, 0.1834, 0.6205, 0.0894],
-                         'half_life':[10.551*365.25*24*3600],
-                         'activity_date':datetime.date(2014, 3, 19),
-                         'activity':1 * 3.7e4},
-                'Co60':{'energy':[1173.228, 1332.492],
-                        'intensity':[0.9985, 0.999826],
-                        'half_life':[1925.28*24*3600],
-                        'actvity_date':datetime.date(2014, 3, 19),
-                        'activity':0.872 * 3.7e4},
-                'Na22':{'energy':[511, 1274.537],
-                        'intensity':[1.80, 0.9994],
-                        'half_life':[2.6018*365.25*24*3600],
-                        'actvity_date':datetime.date(2014, 3, 19),
-                        'activity': 5 * 3.7e4},
-                'Cs137':{'energy':[661.657],
-                         'intensity':[0.851],
-                         'half_life':[30.08*365.25*24*3600],
-                         'actvity_date':datetime.date(2014, 3, 19),
-                         'activity':4.66 * 3.7e4},
-                'Mn54':{'energy':[834.848],
-                        'intensity':[0.99976],
-                        'half_life':[312.20*24*3600],
-                        'actvity_date':datetime.date(2016, 5, 2),
-                        'activity':6.27 * 3.7e4}}
-    decay_lines = {}
-    for nuclide in nuclides:
-        if nuclide in all_decay_lines.keys():
-            decay_lines[nuclide] = all_decay_lines[nuclide]
-        else:
-            raise ValueError(f'{nuclide} not yet added to get_decay_lines()')
-    return decay_lines
+from libra_toolbox.neutron_detection.activation_foils import CheckSource
+
+uCi_to_Bq = 3.7e4
+
+check_source_ba133 = CheckSource(
+    nuclide="Ba133",
+    energy=[80.9979, 276.3989, 302.8508, 356.0129, 383.8485],
+    intensity=[0.329, 0.0716, 0.1834, 0.6205, 0.0894],
+    activity_date=datetime.date(2014, 3, 19),
+    activity=1 * uCi_to_Bq,
+)
+
+check_source_co60 = CheckSource(
+    nuclide="Co60",
+    energy=[1173.228, 1332.492],
+    intensity=[0.9985, 0.999826],
+    activity_date=datetime.date(2014, 3, 19),
+    activity=0.872 * uCi_to_Bq,
+)
+check_source_na22 = CheckSource(
+    nuclide="Na22",
+    energy=[511, 1274.537],
+    intensity=[1.80, 0.9994],
+    activity_date=datetime.date(2014, 3, 19),
+    activity=5 * uCi_to_Bq,
+)
+check_source_cs137 = CheckSource(
+    nuclide="Cs137",
+    energy=[661.657],
+    intensity=[0.851],
+    activity_date=datetime.date(2014, 3, 19),
+    activity=4.66 * uCi_to_Bq,
+)
+check_source_mn54 = CheckSource(
+    nuclide="Mn54",
+    energy=[834.848],
+    intensity=[0.99976],
+    activity_date=datetime.date(2016, 5, 2),
+    activity=6.27 * uCi_to_Bq,
+)
